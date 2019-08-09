d5726738d53e389b32513daf6a25ae55dda40d0b
xiaoyang1919 <1723345514@qq.com>
2019-08-09 11:11:39 +0800

    5

---
+++ test201908090909/TestC2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test201908090909
+{
+    public class TestC2
+    {
+        public int Id { get; set; }
+
+    }
+}
